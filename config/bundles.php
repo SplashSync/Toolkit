ead0de952adafb849282b5f12d905f861876a6ce
BadPixxel <eshop.bpaquier@gmail.com>
2022-06-02 13:41:21 +0200

    WIP: ShippingBo V2

--- config/bundles.php
+++ config/bundles.php
@@ -68,7 +68,7 @@ $connectorsBundles = array(
     Splash\Connectors\Shopify\ShopifyBundle::class => array('all' => true),
     Splash\Connectors\Optilog\OptilogBundle::class => array('all' => true),
     Splash\Connectors\ReCommerce\ReCommerceBundle::class => array('all' => true),
-    //    Splash\Connectors\ShippingBo\ShippingBoBundle::class => array('all' => true),
+    Splash\Connectors\ShippingBo\ShippingBoBundle::class => array('all' => true),
     //    Splash\Connectors\Flat\FlatBundle::class => array('all' => true),
 );
 
