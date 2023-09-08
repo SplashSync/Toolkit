ccc0df318cb9aa0dc0abe8df00bbe07d36c5f807
BadPixxel <eshop.bpaquier@gmail.com>
2023-09-08 14:55:36 +0200

    FIX: Docker Images

--- src/Kernel.php
+++ src/Kernel.php
@@ -13,40 +13,79 @@
  *  file that was distributed with this source code.
  */
 
-namespace App;
+namespace Splash\Toolkit;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
 
 /**
- * Symfony Kernel.
+ * Splash Toolkit Symfony Kernel.
  */
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
-    /** @var string */
-    const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+    /**
+     * This Bundle is Always Active
+     */
+    const BUNDLE_ALL = array("all" => true);
 
     /**
-     * @return string
+     * This Bundle is Only Active in DEV Environments
      */
-    public function getCacheDir(): string
+    const BUNDLE_DEBUG = array('dev' => true, 'test' => true);
+
+    /**
+     * @var null|string
+     */
+    private static ?string $projectDir = null;
+
+    /**
+     * Gets the application root dir (path of the project's composer file).
+     *
+     * @return string The project root dir
+     */
+    public function getProjectDir(): string
     {
-        return $this->getProjectDir().'/var/cache/'.$this->environment;
+        return self::getProjectDirStatic();
     }
 
     /**
-     * @return string
+     * Gets the application root dir (path of the project's composer file).
+     *
+     * @return string The project root dir
+     *
+     * @SuppressWarnings(PHPMD.CamelCaseVariableName)
      */
-    public function getLogDir(): string
+    public static function getProjectDirStatic(): string
     {
-        return $this->getProjectDir().'/var/log';
+        global $_composer_autoload_path;
+
+        if (null === self::$projectDir) {
+            $reflectionObject = new \ReflectionObject(new self("dev", false));
+
+            if (!file_exists($dir = (string) $reflectionObject->getFileName())) {
+                throw new \LogicException(
+                    sprintf('Cannot auto-detect project dir for kernel of class "%s".', $reflectionObject->name)
+                );
+            }
+
+            $dir = $rootDir = realpath($_composer_autoload_path ?? "") ?: $dir;
+            while (!file_exists($dir.'/composer.json') || (false !== strpos((string) $dir, "paddock-core"))) {
+                if (($dir === \dirname($dir))) {
+                    return self::$projectDir = $rootDir;
+                }
+                $dir = \dirname($dir);
+            }
+            self::$projectDir = $dir;
+        }
+
+        return self::$projectDir;
     }
 
     /**
@@ -54,11 +93,19 @@ class Kernel extends BaseKernel
      *
      * @retrun void
      */
-    public function registerBundles()
+    public function registerBundles(): iterable
     {
-        $contents = require $this->getProjectDir().'/config/bundles.php';
-        $dynamics = $this->getDynamicBundles();
-        foreach (array_merge($contents, $dynamics) as $class => $envs) {
+        //==============================================================================
+        // Search for All Active Bundles
+        $bundles = array_merge(
+            $this->getCoreBundles(),
+            $this->getSplashBundles(),
+            require $this->getToolkitResourcesPath()."/bundles/splash.php",
+            $this->getConnectorsBundles(),
+            $this->getDynamicBundles(),
+        );
+
+        foreach ($bundles as $class => $envs) {
             if (isset($envs['all']) || isset($envs[$this->environment])) {
                 /** @phpstan-ignore-next-line  */
                 yield new $class();
@@ -67,24 +114,36 @@ class Kernel extends BaseKernel
     }
 
     /**
-     * Configure Sf Container
+     * Configure Container from Project Config Dir & Toolkit Config Dir
      *
      * @param ContainerBuilder $container
      * @param LoaderInterface  $loader
+     *
+     * @throws Exception
+     *
+     * @return void
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
     {
-        $container->addResource(new FileResource($this->getProjectDir().'/config/bundles.php'));
-        // Feel free to remove the "container.autowiring.strict_mode" parameter
-        // if you are using symfony/dependency-injection 4.0+ as it's the default behavior
-        $container->setParameter('container.autowiring.strict_mode', true);
-        $container->setParameter('container.dumper.inline_class_loader', true);
-        $confDir = $this->getProjectDir().'/config';
-
-        $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{packages}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
+        //==============================================================================
+        // Search for Configs in Project & Toolkit Dirs
+        $confDirs = array(
+            $this->getProjectDir().'/config',
+            $this->getToolkitResourcesPath(),
+        );
+        //==============================================================================
+        // Walk on Config Dirs
+        foreach ($confDirs as $confDir) {
+            if (!is_dir($confDir)) {
+                continue;
+            }
+            $loader->load($confDir.'/{packages}/*.yaml', 'glob');
+            $loader->load($confDir.'/{packages}/'.$this->environment.'/**/*.yaml', 'glob');
+            $loader->load($confDir.'/{services}.yaml', 'glob');
+            $loader->load($confDir.'/{services}_'.$this->environment.'.yaml', 'glob');
+        }
     }
 
     /**
@@ -94,15 +153,61 @@ class Kernel extends BaseKernel
      */
     protected function configureRoutes(RouteCollectionBuilder $routes): void
     {
-        $confDir = $this->getProjectDir().'/config';
+        //==============================================================================
+        // Search for Configs in Project & Toolkit Dirs
+        $confDirs = array(
+            $this->getProjectDir().'/config',
+            $this->getToolkitResourcesPath(),
+        );
+        //==============================================================================
+        // Walk on Config Dirs
+        foreach ($confDirs as $confDir) {
+            if (!is_dir($confDir)) {
+                continue;
+            }
+            $routes->import($confDir.'/{routes}/*.yaml', '/', 'glob');
+            $routes->import($confDir.'/{routes}/'.$this->environment.'/**/*.yaml', '/', 'glob');
+            $routes->import($confDir.'/{routes}.yaml', '/', 'glob');
+        }
+    }
 
-        $routes->import($confDir.'/{routes}/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}'.self::CONFIG_EXTS, '/', 'glob');
+    private function getToolkitResourcesPath(): string
+    {
+        return __DIR__."/Resources/config";
     }
 
     /**
-     * Register App Bundles
+     * Register Toolkit Core Bundles
+     *
+     * @retrun void
+     */
+    private function getCoreBundles(): array
+    {
+        return require $this->getToolkitResourcesPath()."/bundles/core.php";
+    }
+
+    /**
+     * Register Splash Bundles
+     *
+     * @retrun void
+     */
+    private function getSplashBundles(): array
+    {
+        return require $this->getToolkitResourcesPath()."/bundles/splash.php";
+    }
+
+    /**
+     * Register Available Connectors Bundles
+     *
+     * @retrun void
+     */
+    private function getConnectorsBundles(): array
+    {
+        return self::filterBundles(require $this->getToolkitResourcesPath()."/bundles/connectors.php");
+    }
+
+    /**
+     * Register Dynamic Connectors Bundles
      *
      * @retrun void
      */
@@ -110,11 +215,16 @@ class Kernel extends BaseKernel
     {
         $bundles = array();
         //==============================================================================
+        // Safety Check - Connector Dir Exists
+        if (!is_dir($this->getProjectDir()."/connectors")) {
+            return $bundles;
+        }
+        //==============================================================================
         // Search for Bundles in Connectors Dir
         $finder = new Finder();
         $files = $finder
             ->files()
-            ->in(dirname(__DIR__)."/connectors")
+            ->in($this->getProjectDir()."/connectors")
             ->ignoreVCS(true)
             ->depth(array(0,1))
             ->name('*Bundle.php')
@@ -132,7 +242,25 @@ class Kernel extends BaseKernel
             //==============================================================================
             // Register Bundle
             if (class_exists($className)) {
-                $bundles[$className] = array('all' => true);
+                $bundles[$className] = self::BUNDLE_ALL;
+            }
+        }
+
+        return $bundles;
+    }
+
+    /**
+     * Filter Inactive Bundles
+     *
+     * @param array<class-string, array> $bundles
+     *
+     * @return array<class-string, array>
+     */
+    private static function filterBundles(array $bundles): array
+    {
+        foreach (array_keys($bundles) as $class) {
+            if (!class_exists($class)) {
+                unset($bundles[$class]);
             }
         }
 
