e9cc6de44ed803c00b4399c8a5c122a6e46a40b9
BadPixxel <eshop.bpaquier@gmail.com>
2025-03-05 18:25:27 +0100

    WIP: Update of Widget Bundle WIP: Migrate to Twig Component System

--- docker/update.sh
+++ docker/update.sh
@@ -4,10 +4,10 @@
 docker login https://registry.gitlab.com -u BadPixxel
 ########################################################
 # Build Splash Toolkit Alpine Docker Image
-docker build --no-cache -t registry.gitlab.com/splashsync/toolkit:alpine  ./
+docker build --no-cache -t registry.gitlab.com/splashsync/toolkit:3.0  ./
 ########################################################
 # Upload Docker Alpine Image to GitLab
-docker push registry.gitlab.com/splashsync/toolkit:alpine
+docker push registry.gitlab.com/splashsync/toolkit:3.0
 
 ########################################################
 # Build Splash Toolkit Docker Image
